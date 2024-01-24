686efccae0d5a6ac71950649496862d6845f6410
alvaro0219 <alvaroconti05@gmail.com>
2024-01-24 19:33:30 -0300

    manejando mensajes de error

--- blogr/auth.py
+++ blogr/auth.py
@@ -1,15 +1,84 @@
-from flask import Blueprint, render_template
+import functools
+from flask import (
+    Blueprint, render_template, redirect, url_for, flash, request, session, g
+)
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from blogr import db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
-@bp.route('/register')
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Verifica si el usuario ya existe en la base de datos
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user is None:
+            # Crea una nueva instancia de User utilizando el constructor
+            new_user = User(username=username, email=email, password=generate_password_hash(password))
+            
+            # Agrega el nuevo usuario a la base de datos
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('Registro exitoso. Ahora puedes iniciar sesión.', 'success')
+            return redirect(url_for('auth.login'))
+        else:
+            flash(f'El correo {email} ya está registrado', 'danger')
+
     return render_template('auth/register.html')
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Validando datos
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password):
+            # Iniciando sesión
+            session.clear()
+            session['user_id'] = user.id
+            flash('Inicio de sesión exitoso.', 'success')
+            return redirect(url_for('post.posts'))
+        else:
+            flash('Email o contraseña incorrecta', 'danger')
+
     return render_template('auth/login.html')
+
+# Mantener un usuario logueado
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    flash('Sesión cerrada exitosamente.', 'success')
+    return redirect(url_for('home.index'))
+
+# Decorador para requerir inicio de sesión
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            flash('Debes iniciar sesión para acceder a esta página.', 'danger')
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
+
 
 @bp.route('/profile')
 def profile():
