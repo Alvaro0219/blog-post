eb28e5caa178bbcd2b2c9d40224d9ffa790d882d
alvaro0219 <alvaroconti05@gmail.com>
2024-01-24 11:57:36 -0300

    Creando archivo de configuracion

--- blogr/__init__.py
+++ blogr/__init__.py
@@ -5,6 +5,9 @@ def create_app():
     #Crear app de flask
     app = Flask(__name__)
 
+    #Traer archivo de configuracion config.py
+    app.config.from_object('config.Config')
+
     #Registrar vistas
     from blogr import home
     app.register_blueprint(home.bp)
